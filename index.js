83ab40beedd025690fb18b1e187e356d535d0dab
amanprajapati3 <156068199+amanprajapati3@users.noreply.github.com>
2025-05-30 23:28:23 +0530

    modified

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ let menuBar = document.getElementById("menu_bar");
 let menu = document.getElementById("menu");
 let isMenu = false;
 
-menuBar.addEventListener('click', () => {
-    isMenu = !isMenu;
+menuBar.addEventListener("click", () => {
+  isMenu = !isMenu;
 
-    menuBar.src = isMenu ? 'public/close.png' : 'public/menu.png';
+  menuBar.src = isMenu ? "public/close.png" : "public/menu.png";
 
-    if (isMenu) {
-        menu.classList.add("show");
-        menu.classList.remove("hidden");
-    } else {
-        menu.classList.remove("show");
-        setTimeout(() => menu.classList.add("hidden"), 400); // Wait for animation to end
-    }
+  if (isMenu) {
+    menu.classList.add("show");
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.remove("show");
+    setTimeout(() => menu.classList.add("hidden"), 400); // Wait for animation to end
+  }
 });
+
+let faqs = document.querySelectorAll(".faqs");
+
+faqs.forEach((faq) => {
+  let isOpen = false;
+
+  faq.addEventListener("click", () => {
+    const detail = faq.querySelector(".details");
+    const arrow = faq.querySelector(".down_arrow");
+
+    isOpen = !isOpen;
+
+    if (isOpen) {
+      detail.style.display = "block";
+      arrow.style.transform = "rotate(180deg)";
+    } else {
+      detail.style.display = "none";
+      arrow.style.transform = "rotate(0deg)";
+    }
+  });
+});
+
